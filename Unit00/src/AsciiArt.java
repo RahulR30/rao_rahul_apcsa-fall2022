e20a1a2dcf710bbd631c2c096e396e1b774754f4
RahulR30 <sdrahul.rao@gmail.com>
2022-08-17 10:40:27 -0700

    push

--- Unit00/src/AsciiArt.java
+++ Unit00/src/AsciiArt.java
@@ -10,11 +10,11 @@ public class AsciiArt
 {
 	public static void main ( String[] args )
 	{
-		System.out.println("Your Name \n\n" );
-		System.out.println("What type of ANIMAL YOU WILL DRAW" );
+		System.out.println("Rahul Rao \n\n" );
+		System.out.println("CAT" );
 		System.out.println("\n\n\n\n" );
 
-		System.out.println("                /\\                " );
+		System.out.println("                ||                 " );
 		System.out.println("               /  \\               " );
 		System.out.println("              /    \\              " );
 		System.out.println("             [------]             " );
--- Unit00/src/AsciiBox.java
+++ Unit00/src/AsciiBox.java
@@ -8,10 +8,18 @@
 
 public class AsciiBox
 {
-	public static void main(String[] args)
-	{
-		System.out.println("name \t  date \n\n" );
-		System.out.println("+++++++++++++++++++++++++ " );
-
+	public static void main(String[] args) {
+		System.out.println("Rahul Rao\t  8/16/2022 \n\n");
+		for (int x = 0; x < 3; x++) {
+			for (int i = 0; i < 3; i++) {
+				System.out.println("+++++++++++++++++++++++++ ");
+			}
+			for (int i = 0; i < 2; i++) {
+				System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+			}
+		}
+		for (int i = 0; i < 3; i++) {
+			System.out.println("+++++++++++++++++++++++++ ");
+		}
 	}
 }
